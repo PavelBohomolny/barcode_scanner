853181f57ced594c0df90d0c72d98b05e6cfc92d
NexusUA <pbogomolny93@gmal.com>
2022-11-01 13:21:28 +0100

    Add alerts

---
+++ Barcode Scanner/ScannerView.swift
@@ -0,0 +1,46 @@
+//
+//  ScannerView.swift
+//  Barcode Scanner
+//
+//  Created by Pavel Bohomolnyi on 01/11/2022.
+//
+
+import SwiftUI
+
+struct ScannerView: UIViewControllerRepresentable {
+    
+    @Binding var scannedCode: String
+    @Binding var alertItem: AlertItem?
+    
+    func makeUIViewController(context: Context) -> ScannerVC {
+        ScannerVC(scannerDelegate: context.coordinator)
+    }
+    
+    func updateUIViewController(_ uiViewController: ScannerVC, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(scannerView: self)
+    }
+    
+    final class Coordinator: NSObject, ScannerVCDelegate {
+        
+        private let scannerView: ScannerView
+        
+        init(scannerView: ScannerView) {
+            self.scannerView = scannerView
+        }
+        
+        func didFind(barcode: String) {
+            scannerView.scannedCode = barcode
+        }
+        
+        func didSurface(error: CameraError) {
+            switch error {
+            case .invalidDeviceInput:
+                scannerView.alertItem = AlertContext.InvalidDeviceInput
+            case .invalidScannedValue:
+                scannerView.alertItem = AlertContext.invalidScannedType
+            }
+        }
+    }
+}
