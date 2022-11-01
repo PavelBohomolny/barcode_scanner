3f5a0cad8b0c7479929897842465bb1cb4f19041
NexusUA <pbogomolny93@gmal.com>
2022-11-01 13:39:31 +0100

    Refactor, MVVM, Project Organization

---
+++ Barcode Scanner/Screens/BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,49 @@
+//
+//  BarcodeScannerView.swift
+//  Barcode Scanner
+//
+//  Created by Pavel Bohomolnyi on 29/10/2022.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    
+    @StateObject var viewModel = BarcodeScannerViewModel()
+    
+    var body: some View {
+        
+        NavigationStack {
+            
+            VStack {
+                
+                ScannerView(scannedCode: $viewModel.scannedCode,
+                            alertItem: $viewModel.alertItem)
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer().frame(height: 60)
+                
+                Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text(viewModel.statusText)
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(viewModel.statusTextColor)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+            .alert(item: $viewModel.alertItem) { alertItem in
+                Alert(title: Text(alertItem.title),
+                      message: Text(alertItem.message),
+                      dismissButton: alertItem.dismissButton)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarcodeScannerView()
+    }
+}
---
+++ Barcode Scanner/Screens/BarcodeScanner/BarcodeScannerViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  BarcodeScannerViewModel.swift
+//  Barcode Scanner
+//
+//  Created by Pavel Bohomolnyi on 01/11/2022.
+//
+
+import SwiftUI
+
+final class BarcodeScannerViewModel: ObservableObject {
+    
+    @Published var scannedCode = ""
+    @Published var alertItem: AlertItem?
+    
+    var statusText: String {
+        scannedCode.isEmpty ? "Not Yet Scanned" : viewModel.scannedCode
+    }
+    
+    var statusTextColor: Color {
+        scannedCode.isEmpty ? .red : .green
+    }
+}
